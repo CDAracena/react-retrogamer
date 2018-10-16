ff6ef573f3f8ef2647d6b12e52eba8de534ea727
Christian Aracena <CDAracena@gmail.com>
2018-10-16 16:32:13 -0400

    nav scale

--- src/Nav.js
+++ src/Nav.js
@@ -6,18 +6,18 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 const Nav = ({searchFunction, searchTerm})=> (
   <nav className="container-fluid">
-    <ul className="nav-list col-lg-10">
+    <ul className="nav-list col-lg-12">
     <li className="nav-list-item nav-title">AncientGamer</li>
     <li className="nav-list-item"> PlayStation </li>
     <li className="nav-list-item"> Dreamcast </li>
     <li className="nav-list-item"> GameCube </li>
     <li className="nav-list-item">SNES </li>
     <li className="nav-list-item"> Nintendo 64 </li>
+    <li className="nav-list-item">  <div className="searchBar">
+        <input type="text" onChange={searchTerm} className="form-control col-lg-6"/>
+        <button type="submit" onClick={searchFunction} className="btn searchBtn"> Search </button>
+      </div></li>
   </ul>
-  <div className="col-lg-2">
-    <input type="text" onChange={searchTerm}/>
-    <button type="submit" onClick={searchFunction} className="btn"> Search </button>
-  </div>
 </nav>
 )
 
