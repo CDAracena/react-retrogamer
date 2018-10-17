dce4c19394292448460119ca8cf113dee07d338c
Christian Aracena <CDAracena@gmail.com>
2018-10-17 13:43:39 -0400

    dreamcast component, rendering some igdb console data

--- src/App.js
+++ src/App.js
@@ -3,39 +3,44 @@ import Nav from './Nav'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'axios/dist/axios.min.js'
 import './index.css';
+import Dreamcast from './components/Dreamcast';
+import Home from './components/Home';
 
+
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.handleSearchTerm = this.handleSearchTerm.bind(this)
     this.handleSearchClick = this.handleSearchClick.bind(this);
   }
 
-
   state = {
     currentSearchTerm: ''
   }
 
 
-  handleSearchTerm(input){
-    this.setState({currentSearchTerm: input.target.value}, function(){
-
-    })
+  handleSearchTerm(input) {
+    this.setState({
+      currentSearchTerm: input.target.value
+    }, function() {})
   }
-handleSearchClick(){
-  console.log(this.state.currentSearchTerm);
-}
+  handleSearchClick() {}
 
-  render(){
-    return (
+  render() {
+    return (<Router>
       <div className="main-App-container container-fluid">
-
         <Nav searchTerm={this.handleSearchTerm} searchFunction={this.handleSearchClick}/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/dreamcast" component={Dreamcast}/>
 
+        </Switch>
       </div>
-    )
+
+    </Router>)
   }
 }
 
