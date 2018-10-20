44dfbc25b7256691a1a55706bd94c91b0a8ac39a
Christian Aracena <CDAracena@gmail.com>
2018-10-19 23:11:04 -0400

    changed system from state to stateless function, routing match params

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Nav from './Nav'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'axios/dist/axios.min.js'
 import './index.css';
 import System from './components/System';
 import Home from './components/Home';
+import axios from 'axios/dist/axios.min.js';
 
 
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
@@ -15,12 +15,49 @@ class App extends React.Component {
 
     this.handleSearchTerm = this.handleSearchTerm.bind(this)
     this.handleSearchClick = this.handleSearchClick.bind(this);
+    this.getPlatform = this.getPlatform.bind(this)
+    this.getPlatformGames = this.getPlatformGames.bind(this);
+    this.makeAPICalls = this.makeAPICalls.bind(this);
   }
 
   state = {
-    currentSearchTerm: ''
+    currentSearchTerm: '',
+    consoleId: '',
+    proxyUrl: "https://cors-anywhere.herokuapp.com/",
+    systemName: '',
+    systemImg: '',
+    systemGameList: [],
   }
 
+  getPlatform = () => {
+    return axios.get(this.state.proxyUrl + 'https://api-endpoint.igdb.com/platforms/23/', {
+      headers: {
+        "user-key": '371118ee5455adc31af2656760ffe051',
+        Accept: "application/json"
+      }
+    })
+  }
+
+  getPlatformGames = () => {
+    return axios.get(this.state.proxyUrl + 'https://api-endpoint.igdb.com//games/1218', {
+      headers: {
+        "user-key": '371118ee5455adc31af2656760ffe051',
+        Accept: "application/json"
+      }
+    })
+  }
+
+  makeAPICalls(){
+      axios.all([this.getPlatform(), this.getPlatformGames()]).then(axios.spread(function(platform, games) {
+
+        console.log("response1:" + JSON.stringify(platform.data[0].logo.url.replace('thumb', '720p')))
+        this.setState({
+          systemName: platform.data.name,
+          systemImg: platform.data[0].logo.url.replace('thumb', '720p')
+        })
+
+      }.bind(this)))
+  }
 
   handleSearchTerm(input) {
     this.setState({
@@ -35,7 +72,7 @@ class App extends React.Component {
         <Nav searchTerm={this.handleSearchTerm} searchFunction={this.handleSearchClick}/>
         <Switch>
           <Route exact path="/" component={Home}/>
-          <Route path="/dreamcast" component={Dreamcast}/>
+          <Route path="/:id" component={System}/>
 
         </Switch>
       </div>
