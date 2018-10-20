58a49e2fb1122b68a1c5a16bfc2d1262250d7f51
Christian Aracena <CDAracena@gmail.com>
2018-10-20 00:02:19 -0400

    env vars, layed out api call functions, set state for platform game list

--- src/components/System.js
+++ src/components/System.js
@@ -1,24 +1,20 @@
 import React from 'react';
 
-const System = ({systemName, systemImg, match}) => (
+const System = ({systemName, systemImg, match}) => (<div className="container">
+  <div className="row">
+    <div className="col-lg-12 systemHeader">
+      {systemName}
+      <div className="col-lg-12 systemImg">
+        <img src={'https:' + systemImg} alt={systemName}/> {match.params.systemId}
+      </div>
+    </div>
 
-   <div className="container">
-     <div className="row">
-       <div className="col-lg-12 systemHeader">
-         {systemName}
-         <div className="col-lg-12 systemImg">
-           <img src={'https:' + systemImg} alt={systemName}/>
-           {match.params.id}
-         </div>
-       </div>
-
-     </div>
-     <div className="row">
-       <div className="col-lg-4">
-         <ul></ul>
-       </div>
-     </div>
-   </div>
- )
+  </div>
+  <div className="row">
+    <div className="col-lg-4">
+      <ul></ul>
+    </div>
+  </div>
+</div>)
 
 export default System;
